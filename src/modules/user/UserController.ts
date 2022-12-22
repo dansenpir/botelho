9f1e5523868e3b837d4a1967ab4937012867ab2d
Dan <dansenpirdev@gmail.com>
2022-12-22 18:53:46 -0300

    feat: creates delete method on UserController

--- src/modules/user/UserController.ts
+++ src/modules/user/UserController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { UserRepository } from './repositories/UserRepository';
 import { IUser } from '../../interfaces';
 import CreateUser from './useCase/CreateUser';
+import DeleteUser from './useCase/DeleteUser';
 
 const repository = new UserRepository();
 
@@ -14,5 +15,15 @@ export default class UserController {
     await createUser.execute({ id, username });
 
     return res.sendStatus(201);
+  }
+
+  static async delete(req: Request, res: Response) {
+    const id: number = req.body.id;
+
+    const deleteUser = new DeleteUser(repository);
+
+    await deleteUser.execute(id);
+
+    return res.sendStatus(200);
   }
 }
