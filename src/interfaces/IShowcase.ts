af2128570f2cc63f2b0649c5ddce8923c99e73ed
Dan <dansenpirdev@gmail.com>
2022-12-24 17:12:49 -0300

    feat: creates IShowcase

---
+++ src/interfaces/IShowcase.ts
@@ -0,0 +1,9 @@
+import { AccessType, Status } from '@prisma/client';
+
+export interface IShowcase {
+  catalog_service: string;
+  amount: number;
+  access_type: AccessType;
+  id_user: number;
+  status: Status;
+}
