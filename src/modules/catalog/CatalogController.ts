018c1abe62bd344ae3cee1bb75dfebd4977e8eca
Dan <dansenpirdev@gmail.com>
2022-12-24 16:50:20 -0300

    feat: creates CatalogController delete method

--- src/modules/catalog/CatalogController.ts
+++ src/modules/catalog/CatalogController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { CatalogRepository } from './repositories/CatalogRepository';
 import { ICatalog } from '../../interfaces';
 import CreateCatalog from './useCase/CreateCatalog';
+import DeleteCatalog from './useCase/DeleteCatalog';
 
 const repository = new CatalogRepository();
 
@@ -14,5 +15,15 @@ export default class CatalogController {
     await createCatalog.execute({ service, max_amount, supported_access });
 
     return res.sendStatus(201);
+  }
+
+  static async delete(req: Request, res: Response) {
+    const { service } = req.body;
+
+    const deleteCatalog = new DeleteCatalog(repository);
+
+    await deleteCatalog.execute(service);
+
+    return res.sendStatus(200);
   }
 }
