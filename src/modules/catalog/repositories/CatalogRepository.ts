4880f65a49540916657f318d9d4685e41b00cebe
Dan <dansenpirdev@gmail.com>
2022-12-24 15:50:58 -0300

    feat: creates CatalogRepository and create method

---
+++ src/modules/catalog/repositories/CatalogRepository.ts
@@ -0,0 +1,14 @@
+import { ICatalog } from '../../../interfaces';
+import { prisma as db } from '../../../lib/prisma';
+
+export class CatalogRepository {
+  async create({ service, max_amount, supported_access }: ICatalog) {
+    return await db.catalog.create({
+      data: {
+        service,
+        max_amount,
+        supported_access,
+      },
+    });
+  }
+}
