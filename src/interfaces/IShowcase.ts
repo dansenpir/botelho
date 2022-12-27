59726e01ee020bd0cc9c3b1c62584f579387627a
Dan <dansenpirdev@gmail.com>
2022-12-27 15:58:44 -0300

    refactor: moves id from IShowcase to IShowcaseUpdate

--- src/interfaces/IShowcase.ts
+++ src/interfaces/IShowcase.ts
@@ -1,10 +1,13 @@
 import { AccessType, Status } from '@prisma/client';
 
 export interface IShowcase {
-  id: number;
   catalog_service: string;
   amount: number;
   access_type: AccessType;
   id_user: number;
   status: Status;
+}
+
+export interface IShowcaseUpdate extends IShowcase {
+  id: number;
 }
