ccf2c4c75dce6050733603182047b3e6016ce4aa
Dan <dansenpirdev@gmail.com>
2022-12-28 16:20:58 -0300

    feat: creates StakeholderController delete method

--- src/modules/stakeholder/StakeholderController.ts
+++ src/modules/stakeholder/StakeholderController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { StakeholderRepository } from './repositories/StakeholderRepository';
 import { IStakeholder } from '../../interfaces/IStakeholder';
 import CreateStakeholder from './useCase/CreateStakeholder';
+import DeleteStakeholder from './useCase/DeleteStakeholder';
 
 const repository = new StakeholderRepository();
 
@@ -14,5 +15,15 @@ export default class StakeholderController {
     await createStakeholder.execute({ catalog_service, id_user });
 
     return res.sendStatus(201);
+  }
+
+  static async delete(req: Request, res: Response) {
+    const id: number = req.body.id;
+
+    const deleteStakeholder = new DeleteStakeholder(repository);
+
+    await deleteStakeholder.execute(id);
+
+    return res.sendStatus(200);
   }
 }
